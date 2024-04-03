e6d3464e8a45111ebe5a5535d49964b53a130636
Daniel <daniel.gottschalck@lego.com>
2024-04-03 13:41:51 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddMsalAuthentication(options =>
 {
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
 
 await builder.Build().RunAsync();
